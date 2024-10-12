228654b8d5e16e56a7328f0b7cc5505e0cda2040
chaghigdemirjian <cdemi@umich.edu>
2024-10-11 17:53:16 -0700

    Initial Push

---
+++ cd-task-manager/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-xl font-semibold text-center text-blue-600 p-4">this is my app not styling</div>
+  )
+}
+export default App
